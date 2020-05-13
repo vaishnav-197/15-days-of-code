7989d112fb5e4f6ace76b0a463554971b8273b04
vaishnav-197 <46111025+vaishnav-197@users.noreply.github.com>
2020-05-13 23:50:50 +0530

    day 4 -added covid19Py and added covid 19 details

--- vaishnavea18@gmail.com/bot.py
+++ vaishnavea18@gmail.com/bot.py
@@ -1,9 +1,9 @@
 import requests as requests
+from covid import Covid
+import json
+covid = Covid()
 
 url = "https://api.telegram.org/bot1200959445:AAH5rwX_74Z5H6Zdac9u0W-T3T5vV77voVU/"
-reqcovid = requests.get("https://api.covid19api.com/summary")
-responsecovid = reqcovid.json()
-
 
 
 
@@ -39,37 +39,56 @@ def main():
     while True:
         update = last_update(url)
         if update_id == update["update_id"]:
-            if get_message_text(update).lower() == "hi" or get_message_text(update).lower() == "hello" or get_message_text(update).lower() == "/start" :
-                send_message(get_chat_id(update),'Hello welcome , its me Edith ,\n How  do you want me to help you \n 1.Motivation \n 2.Covid details \n 3.Stay tuned for more features.....\n: Do you need Help then press "help"')
+            if get_message_text(update).lower() == "hi" or get_message_text(update).lower() == "hii" or get_message_text(update).lower() == "hello" or get_message_text(update).lower() == "/start" :
+                send_message(get_chat_id(update),'Hello welcome , its me Edith ,\n How  do you want me to help you \n 1.Motivation \n 2.Covid details \n 3.Stay tuned for more features.....\n Do you need Help then press "help"')
             
             elif  get_message_text(update).lower() == "help":
-                send_message(get_chat_id(update)," \n Edith-Help\n For motivation ✨ try \n 1.Motivation\n 2.edith motivate me \n 3.I need motivation \n\n For Covid-19 Details ")
+                send_message(get_chat_id(update)," \n Edith-Help\n For motivation ✨ try \n 1. Motivation\n 2. I need motivation \n\n For Covid-19 Details ")
             
             
-            elif  get_message_text(update).lower() == "motivation " or get_message_text(update).lower() == "i need motivation" or get_message_text(update).lower() == " edith motivate me" or  get_message_text(update).lower() == "1":
+            elif  get_message_text(update).lower() == "motivation " or get_message_text(update).lower() == "i need motivation"  or  get_message_text(update).lower() == "1":
                 mot = requests.get("https://quotes.rest/quote/random?language=en&limit=1")
                 mot = mot.json()
                 send_message(get_chat_id(update),mot['contents']['quotes'][0]['quote'])
-                send_message(get_chat_id(update),"What do you want me to do for you\n  1.Motivation \n 2.Covid details \n 3.Stay tuned for more features.....\n: Do you need Help")
+                send_message(get_chat_id(update)," end motivation What do you want me to do for you\n  1.Motivation \n 2.Covid details \n 3.Stay tuned for more features.....\n: Do you need Help")
             
             
             
             
             elif get_message_text(update).lower() == "Covid details" or  get_message_text(update).lower() == "2" :
-                send_message(get_chat_id(update),"\n Covid-19 Details 🏥 \n Choose from the following Details \n Global Details\n Search By Country ")
-                if get_message_text(update).lower() == "Covid details" :
+                send_message(get_chat_id(update),"\n Covid-19 Details 🏥 \n  Choose from the following Details \n  Global Details\n  Search By Country ")
+                
+                
+            elif get_message_text(update).lower() == "global details" :
+                    reqcovid = requests.get("https://api.covid19api.com/summary")
+                    responsecovid = reqcovid.json()
                     st=''
                     for i in responsecovid['Global'] :
-                        st+= i + " : " + str(responsecovid['Global'][i]) + "\n"
+                        st+= i + " : " +"\n"+ str(responsecovid['Global'][i]) 
                     
                     send_message(get_chat_id(update),"\n Covid-19 Global  Details 🏥 \n  " + st)
-                    
+            elif get_message_text(update).lower() == "country details" :
+                    send_message(get_chat_id(update),"\n  Enter the country name")
+                    country = get_message_text(update).lower()
+                    print(country)
+                    country=covid.get_status_by_country_name(str(country))
+                    send_message(get_chat_id(update)," Country Id: " + country["id"])
+                    send_message(get_chat_id(update)," Country : " + country["country"])
+                    send_message(get_chat_id(update)," confirmed : " + country["confirmed"])
+                    send_message(get_chat_id(update)," active : " + country["active"])
+                    send_message(get_chat_id(update)," deaths : " + country["deaths"])
+                    send_message(get_chat_id(update)," recovered : " + country["recovered"])
+
+
+
+                
+                   
                     
             
             
             
             else :
-                send_message(get_chat_id(update),'sorry command cannot be recognized :(')
+                send_message(get_chat_id(update),'sorry command cannot be recognized ☹️ ☹️')
             update_id += 1
             
 
